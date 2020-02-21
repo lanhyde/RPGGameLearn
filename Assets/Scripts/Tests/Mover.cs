128b3e5d4ee03b220fb52a38cee55ea27c3ecdae
ran kaitoku <k_ran@smile-meister.jp>
2020-02-21 16:09:46 +0900

    follow camera impl

--- Assets/Scripts/Tests/Mover.cs
+++ Assets/Scripts/Tests/Mover.cs
@@ -5,10 +5,22 @@ public class Mover : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
-
+    Ray lastRay;
     // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit hit)) 
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
