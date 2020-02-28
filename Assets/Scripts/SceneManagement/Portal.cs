7964223c1bd8b6a5182dff1157dc443110e29132
ran kaitoku <k_ran@smile-meister.jp>
2020-02-28 15:43:00 +0900

    scene management ver.1

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField]
+        private int sceneToLoad = -1;
+        [SerializeField]
+        private Transform spawnPoint;
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if(portal == this) continue;
+                return portal;
+            }
+            return null;
+        }
+
+        private void UpdatePlayer(Portal portal)
+        {
+            if(portal == null) return;
+            GameObject player = GameObject.FindWithTag("Player");
+            //player.transform.position = portal.spawnPoint.position;
+            player.GetComponent<NavMeshAgent>().Warp(portal.spawnPoint.position);
+            player.transform.rotation = portal.spawnPoint.rotation;
+        }
+    }
+
+}
