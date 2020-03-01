a9da5f3ff27f0aa8c3b47f353db042925307aa88
lanhyde <lanhaide@hotmail.com>
2020-03-01 12:38:54 +0900

    temp impl

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Saving;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }    
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Load()
+        {
+            GetComponent<SavingSystem>().Load(defaultSaveFile);
+
+        }
+
+        private void Save()
+        {
+            GetComponent<SavingSystem>().Save(defaultSaveFile);
+        }
+    }
+
+}
