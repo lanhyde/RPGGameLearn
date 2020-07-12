34b05c9a09f6029b88da966517d4a5ab167822aa
lan hyde <lanhaide@hotmail.com>
2020-07-13 01:41:43 +0900

    script addition, test scene

---
+++ Assets/Scripts/Network/Test/RandomizePosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizePosition : MonoBehaviour
+{
+    public GameObject sphere;
+    private float timer = 0;
+    private float interval = 3f;
+    private Vector3 target = Vector3.zero;
+    private float speed = 1f;
+    void Start()
+    {
+        sphere.transform.position = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), Random.Range(-4, 4));
+        target = new Vector3(Random.Range(-8, 8), Random.Range(-4, 4), 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > interval)
+        {
+            target = new Vector3(Random.Range(-8, 8), Random.Range(-4, 4), 0);
+            timer = 0;
+        }
+        Vector3 direction = target - transform.position;
+        direction.Normalize();
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
